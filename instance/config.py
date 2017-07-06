1a70416fd5a014422aef808b399bcdde5f03cf1e
Leo Brack <leo.brack@hotmail.co.uk>
2017-07-07 00:17:03 +0100

    Working directory system. HTML templates for home about and base pages.

---
+++ instance/config.py
@@ -0,0 +1,8 @@
+# instance/config.py
+
+SECRET_KEY = 'p9Bv<3Eid9%$i01'
+host = "twitter-scraper.cfnapxxrguq5.eu-west-2.rds.amazonaws.com"
+port = 3306
+dbname = "twitterscraper"
+user = "lbrack1"
+password = "5C49d55cd2"
