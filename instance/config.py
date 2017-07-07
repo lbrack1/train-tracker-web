8e3e45905d0caf1fa9141c5067abcd00d516e817
Leo Brack <leo.brack@hotmail.co.uk>
2017-07-07 22:16:38 +0100

    added files to commit

--- instance/config.py
+++ instance/config.py
@@ -1,8 +1,4 @@
 # instance/config.py
 
-SECRET_KEY = 'p9Bv<3Eid9%$i01'
-host = "twitter-scraper.cfnapxxrguq5.eu-west-2.rds.amazonaws.com"
-port = 3306
-dbname = "twitterscraper"
-user = "lbrack1"
-password = "5C49d55cd2"
+SECRET_KEY = 'k&r<st%8e<Uf'
+SQLALCHEMY_DATABASE_URI = 'mysql://leobrack:lb1071@localhost/test_db'
