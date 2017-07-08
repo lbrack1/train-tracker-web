600f1df9f7b4e80209175e9b5d4cb96bcc86fcbd
Leo Brack <leo.brack@hotmail.co.uk>
2017-07-08 18:12:46 +0100

    __init__.py bug fixed

--- app/__init__.py
+++ app/__init__.py
@@ -28,10 +28,6 @@ def create_app(config_name):
         app = Flask(__name__, instance_relative_config=True)
         app.config.from_object(app_config[config_name])
         app.config.from_pyfile('config.py')
-
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
     
     # database intialisation
     db.init_app(app)
