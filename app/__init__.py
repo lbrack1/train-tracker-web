1fc17a202484b5a61b5ae2e4a16ef7e97df009b1
Leo Brack <leo.brack@hotmail.co.uk>
2017-07-08 18:02:08 +0100

    __init__.py bug fixed

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ login_manager = LoginManager()
 
 def create_app(config_name):
 
-    if os.gtenv('FLASK_CONFIG') == "production":
+    if os.getenv('FLASK_CONFIG') == "production":
         app = Flask(__name__)
         app.config.update(
             SECRET_KEY=os.getnev('SECRET_KEY'),
