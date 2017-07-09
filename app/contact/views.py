a7946a6b6c55c6ecbd50dfd646e20641378528ca
Leo Brack <leo.brack@hotmail.co.uk>
2017-07-09 22:47:31 +0100

    Congifured email settings. Contact form messages my email with message

--- app/contact/views.py
+++ app/contact/views.py
@@ -1,9 +1,12 @@
 from flask import Flask, flash, render_template, request
 from forms import ContactForm
-
+from flask.ext.mail import Message, Mail
 from . import contact
 
-# add contact page route                                                                                                                                                             
+mail = Mail()
+
+
+# add contact page route               
 @contact.route('/contact',methods=['GET','POST'])
 def contactpage():
     form = ContactForm()
@@ -12,5 +15,11 @@ def contactpage():
     if form.validate_on_submit():    
         # if valid tell user and redirect to home page 
         flash('Message Sent! Thank you for getting in touch.')
+        msg = Message(form.subject.data, sender='contactform@cryptotracker.co.uk', recipients=['leo.brack@cryptotracker.co.uk'])
+        msg.body = """
+      From: %s <%s>
+      %s
+      """ % (form.name.data, form.email.data, form.message.data)
+        mail.send(msg)
        # return redirect(url_for('contact'))
     return render_template('contact/contact.html', form=form, title='Contact Us')
--- run.py
+++ run.py
@@ -1,11 +1,21 @@
 # run.py
 
 import os
-
+from flask.ext.mail import Mail
 from app import create_app
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
+
+mail = Mail()
+
+app.config["MAIL_SERVER"] = "smtp.zoho.eu"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'contactform@cryptotracker.co.uk'
+app.config["MAIL_PASSWORD"] = 'Contactform'
+
+mail.init_app(app)
 
 if __name__ == '__main__':
     app.run()
