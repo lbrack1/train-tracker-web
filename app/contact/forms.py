bd0354505001fee7bdba6d50698a1a3684a62ef7
Leo Brack <leo.brack@hotmail.co.uk>
2017-07-09 22:15:15 +0100

    Contact form submit button works. Message box size increased and email field checks for valid email entry

--- app/contact/forms.py
+++ app/contact/forms.py
@@ -1,7 +1,7 @@
 # app/contact/forms.py
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, ValidationError, SubmitField
+from wtforms import StringField, ValidationError, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo 
 
 class ContactForm(FlaskForm):
@@ -11,5 +11,5 @@ class ContactForm(FlaskForm):
   name = StringField("Name", validators=[DataRequired()])
   email = StringField("Email", validators=[DataRequired(), Email()])
   subject = StringField("Subject",validators=[DataRequired()])
-  message = StringField("Message",validators=[DataRequired()])
+  message = TextAreaField("Message",validators=[DataRequired()])
   submit = SubmitField("Send")
--- app/contact/views.py
+++ app/contact/views.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, render_template, request
 from forms import ContactForm
 
 from . import contact
 
 # add contact page route                                                                                                                                                             
-@contact.route('/contact')
+@contact.route('/contact',methods=['GET','POST'])
 def contactpage():
     form = ContactForm()
 
     # check form
-    if request.method == 'POST':    
+    if form.validate_on_submit():    
         # if valid tell user and redirect to home page 
         flash('Message Sent! Thank you for getting in touch.')
        # return redirect(url_for('contact'))
