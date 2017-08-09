af2c5a149cacb87df80807c7e6f8d69e3dd17c41
Leo Brack <leo.brack@hotmail.co.uk>
2017-08-09 23:56:27 +0100

    Added shell blog page, changed nav bar so blog shows up when logged in

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,10 +1,22 @@
-# app/blog/views.py                                                                                                                                                                 
+# app/blog/views.py
+                                                                                                                                                                 
 from flask import abort, render_template
 from flask_login import current_user, login_required
 
 from . import blog
 
-# add blog  page                                                                                                                                                                    
+# add blog page                                                                                                                                                                    
 @blog.route('/blog')
 def blogpage():
     return render_template('blog/blog.html', title="Blog")
+
+# add blog detail page
+@blog.route('/blog/detail')
+def blogdetail():
+    return render_template('blog/detail.html', title="Blog")
+
+# add blog edit page
+@blog.route('/blog/edit')
+@login_required
+def blogedit():
+    return render_template('blog/edit.html', title="Edit Blog")
